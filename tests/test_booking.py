a5c049ceb539acfcfb9b2e2da81e712bb6b7ab72
JOSIASBS <josias@cdpjosecabrera.es>
2026-08-24 13:30:52 +0200

    feat: implementar conftest.py con fixtures e integrar tests GET y DELETE

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -41,3 +41,38 @@ def test_create_booking_success():
     assert response_data["booking"]["firstname"] == "Carlos", "Error: El nombre guardado no coincide"
     
     print(f"\n¡Reserva creada con éxito! ID asignado: {response_data['bookingid']}")
+
+def test_get_booking_by_id(created_booking_id):
+
+    """Prueba que se puede consultar una reserva específica pasando su ID."""
+    
+    url = f"{BASE_URL}/booking/{created_booking_id}"
+    headers = {"Accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200, f"Error al consultar reserva: {response.status_code}"
+    response_data = response.json()
+    assert response_data["firstname"] == "Laura"
+    assert response_data["lastname"] == "Tester"
+    print(f"\n¡Reserva {created_booking_id} obtenida correctamente!")
+
+
+def test_delete_booking(auth_token, created_booking_id):
+    """
+    Prueba que se puede eliminar una reserva usando el Token de autorización.
+    """
+    url = f"{BASE_URL}/booking/{created_booking_id}"
+    
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Cookie": f"token={auth_token}"
+    }
+
+   
+    response = requests.delete(url, headers=headers)
+
+    
+    assert response.status_code in [200, 201], f"Error al eliminar la reserva: {response.status_code}"
+    print(f"\n¡Reserva {created_booking_id} eliminada con éxito!")
